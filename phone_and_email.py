89b39e1e84ebaae60f19978e55029e35155565bc
Kyle Kauma <kylekauma@gmail.com>
2021-09-02 15:21:00 -0500

    add logic for finding matches in clipboard text

--- phone_and_email.py
+++ phone_and_email.py
@@ -20,6 +20,17 @@ email_regex = re.compile(r"""
 	(\.[a-zA-z]{2,4})				# dot-something
 	)""", re.VERBOSE)
 
+# Find matches in clipboard text
+text = str(pyperclip.paste())
+matches []
+for groups in phone_regex.findall(text):
+	phone_num = "-".join([groups[1], groups[3], groups[5]])
+	if groups[8] != "":
+		phone_num += " x" + groups[8]
+	matches.append(phone_num)
+for groups in email_regex.findall(text):
+	matches.append(groups[0])
+
 
 
 
